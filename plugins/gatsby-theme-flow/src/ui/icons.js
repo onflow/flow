e68c1a23614b602b43d806ec409b02ccf9787dfb
Peter Siemens <peterjsiemens@gmail.com>
2020-09-01 12:51:41 -0700

    Add project icons to sidebar

--- plugins/gatsby-theme-flow/src/ui/icons.js
+++ plugins/gatsby-theme-flow/src/ui/icons.js
@@ -6,7 +6,21 @@ import {
 } from 'react-icons/fa';
 
 import Search from '../assets/search.svg';
-import Run from '../assets/run.svg';
+
+import Cadence from '../assets/cadence.svg';
+import CadenceColor from '../assets/cadence-color.svg';
+import Emulator from '../assets/emulator.svg';
+import EmulatorColor from '../assets/emulator-color.svg';
+import CLI from '../assets/cli.svg';
+import CLIColor from '../assets/cli-color.svg';
+import Playground from '../assets/playground.svg';
+import PlaygroundColor from '../assets/playground-color.svg';
+import GoSDK from '../assets/go.svg';
+import GoSDKColor from '../assets/go-color.svg';
+import JSSDK from '../assets/js.svg';
+import JSSDKColor from '../assets/js-color.svg';
+import VSCode from '../assets/vscode.svg';
+import VSCodeColor from '../assets/vscode-color.svg';
 
 export {
   FaArrowDown as IconArrowDown,
@@ -27,21 +41,17 @@ export {
   FaUmbrellaBeach as IconPlayground,
 } from 'react-icons/fa';
 
-const IconContainer = styled.img({
+const SVGIconContainer = styled.img({
   height: '1em',
 });
 
-export function IconSearch() {
-  return (
-    <IconContainer src={Search} />
+function wrapSVGIcon(icon) {
+  return () => (
+    <SVGIconContainer src={icon} />
   )
 };
 
-export function IconRun() {
-  return (
-    <IconContainer src={Run} />
-  )
-};
+export const IconSearch = wrapSVGIcon(Search);
 
 const StackedIconContainer = styled.div({
   display: 'flex',
@@ -65,4 +75,39 @@ export function IconCollapseList() {
       <FaChevronUp />
     </StackedIconContainer>
   )
+};
+
+const projectIcons = {
+  'cadence': {
+    default: Cadence,
+    color: CadenceColor,
+  },
+  'emulator': {
+    default: Emulator,
+    color: EmulatorColor,
+  },
+  'cli': {
+    default: CLI,
+    color: CLIColor,
+  },
+  'playground': {
+    default: Playground,
+    color: PlaygroundColor,
+  },
+  'go-sdk': {
+    default: GoSDK,
+    color: GoSDKColor,
+  },
+  'js-sdk': {
+    default: JSSDK,
+    color: JSSDKColor,
+  },
+  'vscode': {
+    default: VSCode,
+    color: VSCodeColor,
+  },
+};
+
+export function getProjectIcon(name) {
+  return projectIcons[name];
 };
