caf503c12744f2b614ef49acd9aa84b3303bee3b
10thfloor <remember.mackenzie@gmail.com>
2020-09-01 12:03:15 -0700

    Finish porting Cadence Hello World with images from GCP.

---
+++ content/components/tutorial-image.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import styled from "@emotion/styled";
+// import { colors, theme } from "gatsby-theme-flow/src/colors";
+// import { size } from "polished";
+
+export default function TutorialImage(props) {
+  return <img src={props.src} />;
+}
