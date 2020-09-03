45832f513046778521e7d2ef65c1882c22809046
Bastian Müller <bastian@axiomzen.co>
2020-09-03 12:39:50 -0700

    add grammars for all used languages

--- plugins/gatsby-theme-flow/gatsby-node.js
+++ plugins/gatsby-theme-flow/gatsby-node.js
@@ -4,6 +4,22 @@ const git = require("simple-git")();
 const { createFilePath } = require("gatsby-source-filesystem");
 const { createPrinterNode } = require("gatsby-plugin-printer");
 
+// // Replacing '/' would result in empty string which is invalid
+// const replacePath = path => (path === `/` ? path : path.replace(/\/$/, ``))
+// // Implement the Gatsby API “onCreatePage”. This is
+// // called after every page is created.
+// exports.onCreatePage = ({ page, actions }) => {
+//   const { createPage, deletePage } = actions
+//   const oldPage = Object.assign({}, page)
+//   // Remove trailing slash unless page is /
+//   page.path = replacePath(page.path)
+//   if (page.path !== oldPage.path) {
+//     // Replace old page with new page
+//     deletePage(oldPage)
+//     createPage(page)
+//   }
+// }
+
 exports.onCreateNode = async function (
   {node, actions, getNode, loadNodeContent},
   {
