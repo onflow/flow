bc7130e077e6566fde925b5754eb05d74660bb8b
vishal <1117327+vishalchangrani@users.noreply.github.com>
2020-11-02 16:18:10 -0800

    updating operator index page

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -334,7 +334,7 @@ const sections = [
           "operator/node-setup",
           "operator/node-bootstrap",
           "operator/monitoring-nodes",
-          "operator/FAQs",
+          "operator/faq",
       ],
     //  Staking: [
     //````  ],
