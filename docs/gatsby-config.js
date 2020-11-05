8ebe91392764efa7a00a1fd0a4eaaae0431fb151
Vishal <1117327+vishalchangrani@users.noreply.github.com>
2020-11-05 13:47:42 -0800

    changing order of documents under node-operation (#152)

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -201,8 +201,8 @@ const sections = [
     sidebar: {
       "Overview": [
         "node-operation/index",
-        "node-operation/node-roles",
         "node-operation/node-setup",
+        "node-operation/node-roles",
       ],
       "Operator Guide": [
         "node-operation/node-bootstrap",
