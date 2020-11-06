40f3e97d27aa5f307ec68aea4ed1267dc719d0ec
Peter Siemens <peterjsiemens@gmail.com>
2020-11-05 19:51:34 -0800

    Fix webpack warnings

--- docs/content/components/tutorial-image.js
+++ docs/content/components/tutorial-image.js
@@ -6,7 +6,7 @@ import React from "react";
 export default function TutorialImage(props) {
   return (
     <div style={{ marginBottom: "1rem" }}>
-      <img src={props.src} />
+      <img src={props.src} alt="" />
     </div>
   );
 }
--- docs/plugins/gatsby-theme-flow/src/components/sidebar-nav.js
+++ docs/plugins/gatsby-theme-flow/src/components/sidebar-nav.js
@@ -211,7 +211,6 @@ function getStylesForNavItem(page) {
       };
     default:
       return {};
-      break;
   }
 }
 
