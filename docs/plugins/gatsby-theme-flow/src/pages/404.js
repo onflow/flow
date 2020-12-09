0ef3a9ed723ad4fe250d40efdcc61b3fab248b45
10thfloor <remember.mackenzie@gmail.com>
2020-12-09 15:08:55 -0800

    Improved 404 page.

--- docs/plugins/gatsby-theme-flow/src/pages/404.js
+++ docs/plugins/gatsby-theme-flow/src/pages/404.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React from "react";
 
 export default function NotFound() {
-  return <div>not found</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1
+        style={{
+          padding: "2rem",
+        }}
+      >
+        Sorry, this page does not exist.
+      </h1>
+      <img
+        styles={{ display: "block", width: "400px" }}
+        src="https://port.onflow.org/static/media/logogif.c74463f5.gif"
+      />
+    </div>
+  );
 }
