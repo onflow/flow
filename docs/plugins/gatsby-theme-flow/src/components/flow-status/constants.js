8a3a71db8b109bfdb873a55e5872078249122d98
Mackenzie <remember.mackenzie@gmail.com>
2021-06-08 16:20:18 -0700

    Update constants

--- docs/plugins/gatsby-theme-flow/src/components/flow-status/constants.js
+++ docs/plugins/gatsby-theme-flow/src/components/flow-status/constants.js
@@ -5,5 +5,8 @@ export const HEALTHY = "HEALTHY";
 export const DEGRADED = "DEGRADED";
 export const UNAVAILABLE = "UNAVAILABLE";
 export const FORUM_ADDRESS = "https://forum.onflow.org/t/";
+export const MAINNET_ACCESS_API_URL = "https://access-mainnet-beta.onflow.org";
+export const TESTNET_ACCESS_API_URL = "https://access-testnet.onflow.org";
+export const CANARYNET_ACCESS_API_URL = "https://canary.onflow.org";
 export const BREAKING_CHANGES_RESOURCE =
   "https://forum.onflow.org/c/announcements/breaking-changes/30.json";
