5c740fda1ce604dcca537f02853145b786fc9899
10thfloor <remember.mackenzie@gmail.com>
2021-06-09 11:39:52 -0700

    Adds mixpanel tracking to status page.

--- docs/plugins/gatsby-theme-flow/src/components/flow-status/components/recentPost.js
+++ docs/plugins/gatsby-theme-flow/src/components/flow-status/components/recentPost.js
@@ -1,12 +1,23 @@
 import React from "react";
 
+import { useMixpanel } from "gatsby-plugin-mixpanel";
+
 import { FORUM_ADDRESS } from "../constants";
 import { AnnouncementCardWrapper } from "../styles";
 import { IconExternalLink } from "../../../ui/icons";
 
 export default function RecentPost({ post }) {
+  const mixpanel = useMixpanel();
   return (
-    <a href={FORUM_ADDRESS + post.slug} target="_blank">
+    <a
+      href={FORUM_ADDRESS + post.slug}
+      target="_blank"
+      onClick={() =>
+        mixpanel.track("status_page_forum_post_clicked", {
+          post: post.slug,
+        })
+      }
+    >
       <AnnouncementCardWrapper>
         <div>
           <h3>{post.fancy_title}</h3>
