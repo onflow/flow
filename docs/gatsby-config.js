8730be1c1135a5a5f4a46b3ba182ccef7db6e969
10thfloor <remember.mackenzie@gmail.com>
2021-09-16 09:10:57 -0700

    Replace missing FCL side-menu.

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -377,7 +377,21 @@ const sections = [
     patterns: ["docs/**/*", "packages/**/*"],
     sidebarAlwaysExpanded: true,
     sidebar: {
-      null: ["[Home](/)"]
+      null: ["[Home](/)"],
+      "API Reference": [
+        "[Configuration](/fcl/api#configuration)",
+        "[Wallet Interactions](/fcl/api#wallet-interactions)",
+        "[On-Chain Interactions](/fcl/api#on-chain-interactions)",
+        "[Types, Interfaces, and Definitions](/fcl/api#types-interfaces-and-definitions)"
+      ],
+      Changelogs: [
+        "[@onflow/fcl](/fcl/packages/fcl/CHANGELOG)",
+        "[@onflow/types](/fcl/packages/types/CHANGELOG)"
+      ],
+      "Developer Guides": [
+        "[Introducing @onflow/fcl](/fcl)",
+        "[Flow App Quickstart](/fcl/flow-app-quickstart)"
+      ]
     }
   },
   {
