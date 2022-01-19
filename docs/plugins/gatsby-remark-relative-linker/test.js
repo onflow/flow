8e91c1f1b0388260f9bd77c87a2608d69b326865
10thfloor <remember.mackenzie@gmail.com>
2022-01-18 17:07:34 -0800

    Cleanup whitespace, add missing arg.

--- docs/plugins/gatsby-remark-relative-linker/test.js
+++ docs/plugins/gatsby-remark-relative-linker/test.js
@@ -13,54 +13,43 @@ test("Handles unprefixed links", (t) => {
 test("Updates relative depth 0", (t) => {
   const initial = "./hello.md";
   const expected = "../hello.md";
-
   const result = updateRelativeDepth(initial);
-
   t.is(result, expected);
 });
 
 test("Updates relative depth 1", (t) => {
   const initial = "../hello.md";
   const expected = "../../hello.md";
-
   const result = updateRelativeDepth(initial);
-
   t.is(result, expected);
 });
 
 test("Updates relative depth 2", (t) => {
   const initial = "../../hello.md";
   const expected = "../../../hello.md";
-
   const result = updateRelativeDepth(initial);
-
   t.is(result, expected);
 });
 
 test("Handles unprefixed routes in index pages", (t) => {
   const initial = "hello.md";
   const expected = "./hello.md";
-
   const isIndex = true;
   const result = updateRelativeDepth(initial, isIndex);
-
   t.is(result, expected);
 });
 
 test("Handles other routes in index pages, depth 0", (t) => {
   const initial = "./hello.md";
   const expected = "./hello.md";
-
   const result = updateRelativeDepth(initial);
-
   t.is(result, expected);
 });
 
 test("Handles other routes in index pages", (t) => {
   const initial = "../hello.md";
   const expected = "../hello.md";
-
-  const result = updateRelativeDepth(initial);
-
+  const isIndex = true;
+  const result = updateRelativeDepth(initial, isIndex);
   t.is(result, expected);
 });
